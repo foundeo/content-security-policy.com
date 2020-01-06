718d0fd4306cc2109428e34f6cff7f92a8e50728
Pete Freitag <pete@foundeo.com>
2020-01-06 16:46:49 -0500

    Add CSP3 test

--- www/js/browser-test.js
+++ www/js/browser-test.js
@@ -2,3 +2,8 @@ window.addEventListener('load', function() {
 	document.getElementById('userAgent').innerText = window.navigator.userAgent;
 	
 });
+function runUnsafeHashNode() {
+	var node = document.getElementById('unsafeHashNode');
+	node.className='alert alert-success';
+	node.innerText='<h3><span class="glyphicon glyphicon-ok"></span> CSP Level 3 Unsafe Hashes Supported</h3>';
+}
