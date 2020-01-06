1f48954120c015651f0b65ccbd663eaa7f3cfb84
Pete Freitag <pete@foundeo.com>
2020-01-06 16:59:18 -0500

    Fix CSP3 js

--- www/js/browser-test.js
+++ www/js/browser-test.js
@@ -5,5 +5,5 @@ window.addEventListener('load', function() {
 function runUnsafeHashNode() {
 	var node = document.getElementById('unsafeHashNode');
 	node.className='alert alert-success';
-	node.innerText='<h3><span class="glyphicon glyphicon-ok"></span> CSP Level 3 Unsafe Hashes Supported</h3>';
+	node.innerHTML='<h3><span class="glyphicon glyphicon-ok"></span> CSP Level 3 Unsafe Hashes Supported</h3>';
 }
