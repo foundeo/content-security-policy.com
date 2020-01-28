6fe70c68c9f4c6a0f8d0777e6a90a9b2c4525d6c
Pete Freitag <pete@foundeo.com>
2020-01-27 20:40:26 -0500

    Add sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("www/css");
   eleventyConfig.addPassthroughCopy("www/js");
   eleventyConfig.addPassthroughCopy("www/favicon.ico");
   eleventyConfig.addPassthroughCopy("www/robots.txt");
+
+  //used for sitemap
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+
   return {
   	passthroughFileCopy: true,
     dir: {
