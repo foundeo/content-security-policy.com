2f24779b08dee6ca2bd6fa05565ab48a829b75d7
Pete Freitag <pete@foundeo.com>
2021-11-23 10:44:16 -0500

    fix csp tests

--- www/js/browser-test.js
+++ www/js/browser-test.js
@@ -7,3 +7,15 @@ function runUnsafeHashNode() {
 	node.className='alert alert-success';
 	node.innerHTML='<h3><i class="fa fa-check" aria-hidden="true"></i>	CSP Level 3 Unsafe Hashes Supported</h3>';
 }
+
+function runHashTest() {
+	window.addEventListener('load', function() {
+		var hashNode = document.getElementById('hashNode');
+		var jsNode = document.getElementById("jsNode");
+		if (jsNode.className == 'alert alert-danger') {
+		  return;
+		}
+		hashNode.className='alert alert-success';
+		hashNode.innerHTML = '<h3><i class="fa fa-check" aria-hidden="true"></i> CSP Level 2 Inline Hash Supported</h3>';
+	});
+}
