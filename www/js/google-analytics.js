213d05b2d539d2cb70d7bcacfe35934c90ce1af4
Pete Freitag <pete@foundeo.com>
2023-11-27 15:12:48 -0500

    Update google analytics

--- www/js/google-analytics.js
+++ www/js/google-analytics.js
@@ -1,9 +1,4 @@
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-82573-19']);
-  _gaq.push(['_setDomainName', 'content-security-policy.com']);
-  _gaq.push(['_trackPageview']);
-(function() {
- var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
- var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-3GD6TMCQ54');
