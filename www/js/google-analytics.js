01ba7363a45bfa6dc99b75421e86b013137c74ee
Pete Freitag <pete@foundeo.com>
2024-01-05 11:03:39 -0500

    update twitter

--- www/js/google-analytics.js
+++ www/js/google-analytics.js
@@ -2,3 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-3GD6TMCQ54');
+
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','oiuel');
